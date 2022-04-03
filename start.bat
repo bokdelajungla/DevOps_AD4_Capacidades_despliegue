2e502ed18473dfcead5beafaf76cfcc53b11efcb
bokdelajungla <bokdelajungla@hotmail.com>
2022-04-03 23:35:40 +0200

    start.bat changes to remove filename

--- start.bat
+++ start.bat
@@ -4,10 +4,9 @@ set /A puerto=12345
 
 :loop
 if "%1" == "" goto done
-if "%1" == "-f" set "fichero=%2"
 if "%1" == "-p" set "puerto=%2"
 shift
 goto :loop
 
 :done
-python server.py -f %fichero% -p %puerto%
+python server.py -p %puerto%
